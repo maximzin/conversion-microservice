f74228a1c2bc9650d940fcb477316a0a868a199f
maxim zinovev <maximzin@mail.ru>
2026-06-24 17:18:34 +0500

    first commit

---
+++ src/main/java/com/zinoviev/conversion_microservice/ConversionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.zinoviev.conversion_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversionMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zinoviev/conversion_microservice/ConversionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zinoviev.conversion_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
